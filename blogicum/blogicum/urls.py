0e88b236fa85eff3d64e20f33735d8412abdce57
Stepan Pomogaibo <haroshij@yandex.ru>
2024-08-21 11:19:51 +0300

    first day commit

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('pages/', include('pages.urls')),
+    path('', include('blog.urls')),
+    # path('posts/<int:id>/', include('blog.urls')),
+    # path('category/<slug:category_slug>/', include('blog.urls')),
 ]
